25d5841833cc1207636bc6940c79e74d29bd8821
vgaupset <vgaupset@gmail.com>
2023-10-14 19:44:43 +0200

    last change

--- k8s/run.sh
+++ k8s/run.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
+
+kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/master/deploy/local-path-storage.yaml
+
+kubectl patch storageclass local-path -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
+
 kubectl apply -f kube-flannel.yml
 
 kubectl taint nodes --all  node-role.kubernetes.io/control-plane-
